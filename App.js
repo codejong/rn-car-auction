86dd6b5e7cb40dcb94fa0bb21820e568747e2e1c
Jong <yomybaby@yomybaby.com>
2018-12-17 13:43:30 +0900

    StackNavigation 단계 1

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+// In App.js in a new project
 
-export default class App extends React.Component {
+import React from 'react';
+import { View, Text } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center' }}>
-        <Text>안녕하세요.</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
       </View>
     );
   }
 }
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+});
+
+export default createAppContainer(AppNavigator);
